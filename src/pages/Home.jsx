a1b206cbcd286c7722e0fadead5060ba022e9ee9
Sharvan <sharvangupta999036@gmail.com>
2026-03-20 15:57:48 +0530

    Image changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
-import shreeJi from '../assets/shree ji .jpeg'
+import shreeJi from '../assets/shreeji.png'
 import BusinessCard from '../components/BusinessCard'
 
 // Animated counter hook
